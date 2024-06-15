8753c2c5be15c11bcd490baea16dd46f24d39edb
Nathan John <codesnj@gmail.com>
2024-06-15 13:29:37 +0100

    update

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -12,20 +12,29 @@ const iconMap = {
   pending: ClockIcon,
   invoices: InboxIcon,
 };
-
-export default async function CardWrapper() {
+import { FormattedCustomersTable } from '@/app/lib/definitions';
+export default async function CardWrapper({
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+}: {
+    numberOfInvoices: FormattedCustomersTable[];
+    numberOfCustomers: FormattedCustomersTable[];
+    totalPaidInvoices: FormattedCustomersTable[];
+    totalPendingInvoices: FormattedCustomersTable[];
+}) {
+  
   return (
     <>
-      {/* NOTE: comment in this code when you get to this point in the course */}
-
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      /> 
     </>
   );
 }
